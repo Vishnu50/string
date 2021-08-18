9a9748adc7dff92653cc4c49251934a9c43901f8
vishnu <vishnu.v>
2021-08-18 19:22:14 +0530

    code change

---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+
+Public class Main {
+	public static void Main (string[] args) {
+	double my gpa=8.0;
+	string my firstname="vishnu";
+	string my lastname="venkat";
+	system.out.println(my firstname+ "" +my lastname)
+	}
